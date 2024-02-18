ed18c218ab91f12414198ab1a886c5fdf70a515d
shahirsizan <shahir.sizan18@gmail.com>
2024-02-18 22:35:49 +0600

    ALmost finished. Just a few minor details are pending

--- script.js
+++ script.js
@@ -5,15 +5,32 @@ const searchBox = document.getElementById("search-box");
 const searchResult = document.getElementById("search_result");
 const showMoreBtn = document.getElementById("show-more-btn");
 
-let keyword = "";
-let page = 1;
+//let keyword = "";
+//let page = 1;
 
 async function searchImages() {
-	keyword = searchBox.value;
-	const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}`;
+	let keyword = searchBox.value;
+	const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
 	const response = await fetch(url);
 	const data = await response.json();
 	console.log(data);
+	const results = data.results;
+
+    if(page == 1){  // If new search, then clear the innerHTML and then proceed.
+        searchResult.innerHTML = "";
+    }
+
+	results.map((result) => {
+		const image = document.createElement("img");
+		image.src = result.urls.small;
+		const imageLink = document.createElement("a");
+		imageLink.href = result.links.html;
+		imageLink.target = "_blank";
+
+		imageLink.appendChild(image);
+		searchResult.appendChild(imageLink);
+	});
+	showMoreBtn.style.display = "block";
 }
 
 searchBox.addEventListener("keydown", (event) => {
@@ -23,3 +40,9 @@ searchBox.addEventListener("keydown", (event) => {
 		searchImages();
 	}
 });
+
+showMoreBtn.addEventListener("click", ()=>{
+    page++;
+    searchImages();
+
+})
